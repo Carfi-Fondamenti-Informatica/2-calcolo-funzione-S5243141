b7abe884bd6387a4c9a92e5610d8b18dcd1234a0
S5243141 <99831210+S5243141@users.noreply.github.com>
2022-03-30 19:03:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int main(){
    float a=0, b=0, x=0, y=0;
    float z;
+   
    cin >> a >> b >> x >> y;
+   
    if (x<0 && y>0) {
        z = a*x-b*y;
    } else if (x>=0 && y<=0) {
