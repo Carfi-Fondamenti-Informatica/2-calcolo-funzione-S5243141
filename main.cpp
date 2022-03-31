2f512a3de0a7e99b1eb8f07d239e64eb2b50f83a
S5243141 <99831210+S5243141@users.noreply.github.com>
2022-03-31 13:43:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,11 @@ int main(){
    cin >> a >> b >> x >> y;
    
    if ((x<0) && (y>0)) {
-       z = a*x-b*y;
+       z -= a*x-b*y;
    } else if ((x>=0) && (y<=0)) {
-       z = a*(x*x)-b*y;
+       z -= a*(x*x)-b*y;
    } else {
-       z = a*x+b*(y*y);
+       z += a*x+b*(y*y);
    }
    cout << z << endl;
    return 0;
